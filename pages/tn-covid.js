9470566e619b1e399af78e648a2b49597936c440
Adrian Selva <adrianjselva@gmail.com>
2020-06-10 13:06:28 -0700

    Added the ability to view testing data and updated data for 06/10/2020

--- pages/tn-covid.js
+++ pages/tn-covid.js
@@ -14,6 +14,8 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import CountyScroll from '../components/county_scroll'
 
 import CountyData from '../assets/counties.json'
+import MapData from '../assets/maps.json'
+import GeoJSON from '../assets/county_geojson.json'
 
 const PLOT_COMPONENT = {
   plotly: dynamic(import('../components/plot'), {
@@ -32,7 +34,10 @@ class TNCovid extends React.Component {
       type: 'total_cases',
       county: '',
       data: null,
-      layout: null
+      layout: null,
+      map_type: 'daily_cases_map',
+      map_data: this.plotObject(MapData['daily_cases_map']).data,
+      map_layout: this.plotObject(MapData['daily_cases_map']).layout
     };
   }
 
@@ -48,6 +53,10 @@ class TNCovid extends React.Component {
       return this.totalPlot(obj.xval, obj.yval, obj.linecolor, obj.gtitle, obj.ytitle);
     } else if(obj.type == "daily") {
       return this.dailyPlot(obj.xval, obj.yval, obj.barcolor, obj.fillcolor, obj.movingAverage, obj.movingLineColor, obj.gtitle, obj.ytitle);
+    } else if(obj.type == "testing") {
+      return this.testPlot(obj.xval, obj.totalTestVal, obj.positiveVals, obj.percentPositive, obj.gtitle)
+    } else if(obj.type == "map") {
+      return this.countyMap(obj.fips, obj.z)
     } else {
       return {
         data: {},
@@ -126,6 +135,73 @@ class TNCovid extends React.Component {
     }
   }
 
+  testPlot(xval, totalTestVal, positiveVals, percentPositive, gtitle) {
+    let data = [{
+      x: xval,
+      y: totalTestVal,
+      type: 'bar',
+      name: 'Total Tests',
+      hoverinfo: 'x+y',
+      marker: {
+        color: 'rgb(206, 162, 219)'
+      }
+    },
+    {
+      x: xval,
+      y: positiveVals,
+      type: 'bar',
+      name: 'Positive Tests',
+      hoverinfo: 'x+y',
+      marker: {
+        color: 'rgb(0, 182, 199)'
+      }
+    },
+    {
+      x: xval,
+      y: percentPositive,
+      type: 'scatter',
+      mode: 'lines',
+      name: 'Positive (%)',
+      yaxis: 'y2',
+      hoverinfo: 'x+y',
+      line: {
+        color: 'rgb(191, 23, 23)'
+
+      }
+    }];
+
+    let layout = {
+      xaxis: {
+        title: 'Date'
+      },
+      yaxis: {
+        title: 'Daily Tests'
+      },
+      yaxis2: {
+        overlaying: "y",
+        side: "right",
+        title: "Positive (%)",
+        rangemode: 'tozero',
+        showgrid: false
+      },
+      barmode: 'overlay',
+      title: gtitle,
+      legend: {
+        y: -.3,
+        orientation: 'h'
+      },
+      margin: {
+        r: 45
+      }
+    }
+
+    return {
+      data: data,
+      layout: layout
+    }
+
+  }
+
   changePlots(type) {
     if(this.state.type == type) {
       return;
@@ -143,7 +219,6 @@ class TNCovid extends React.Component {
   }
 
   updateCounty(county) {
-    console.log(this.state)
     let cData = CountyData[county][this.state.type];
 
     let gObj = this.plotObject(cData);
@@ -164,6 +239,47 @@ class TNCovid extends React.Component {
     }
   }
 
+  menuClick(type) {
+    if(this.state.county == '') {
+      this.setState({
+        type: type,
+        county: this.state.county,
+        data: this.state.data,
+        layout: this.state.layout
+      })
+    } else {
+      this.changePlots(type)
+    }
+  }
+
+  countyMap(fips, z) {
+    let data = [{
+      type: "choroplethmapbox",
+      geojson: GeoJSON,
+      locations: fips,
+      z: z,
+      featureidkey: "properties.FIPS",
+      colorscale: 'Viridis'
+    }];
+
+    let layout = {
+      mapbox: {
+        style: "carto-positron",
+        zoom: 5,
+        center: {
+          lat: 35.51,
+          lon: -86
+        }
+      },
+      title: "Daily Confirmed COVID-19 Cases"
+    }
+
+    return {
+      data: data,
+      layout: layout
+    }
+  }
+
 
   render() {
     return (
@@ -174,17 +290,17 @@ class TNCovid extends React.Component {
             <Col md="auto">
             <Tab.Container id="graph-selection" defaultActiveKey="total_cases">
               <ListGroup>
-                <ListGroup.Item action onClick={() => this.changePlots("total_cases")} eventKey="total_cases">Total Cases</ListGroup.Item>
-                <ListGroup.Item action onClick={() => this.changePlots("daily_cases")} eventKey="daily_cases">Daily Cases</ListGroup.Item>
-                <ListGroup.Item action onClick={() => this.changePlots("total_deaths")} eventKey="total_deaths">Total Deaths</ListGroup.Item>
-                <ListGroup.Item action onClick={() => this.changePlots("daily_deaths")} eventKey="daily_deaths">Daily Deaths</ListGroup.Item>
-                <ListGroup.Item action eventKey="testing">Testing Data</ListGroup.Item>
-                <ListGroup.Item action onClick={() => this.changePlots("active_cases")} eventKey="active_cases">Active Cases</ListGroup.Item>
-                <ListGroup.Item action onClick={() => this.changePlots("daily_active")} eventKey="daily_active">Daily Active Cases</ListGroup.Item>
-                <ListGroup.Item action onClick={() => this.changePlots("total_recoveries")} eventKey="total_recoveries">Total Recoveries</ListGroup.Item>
-                <ListGroup.Item action onClick={() => this.changePlots("daily_recoveries")} eventKey="daily_recoveries">Daily Recoveries</ListGroup.Item>
-                <ListGroup.Item action onClick={() => this.changePlots("total_hospitalized")} eventKey="total_hospitalized">Total Hospitalized</ListGroup.Item>
-                <ListGroup.Item action onClick={() => this.changePlots("daily_hospitalized")} eventKey="daily_hospitalized">Daily Hospitalized</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("total_cases")} eventKey="total_cases">Total Cases</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("daily_cases")} eventKey="daily_cases">Daily Cases</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("total_deaths")} eventKey="total_deaths">Total Deaths</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("daily_deaths")} eventKey="daily_deaths">Daily Deaths</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("testing")} eventKey="testing">Testing Data</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("active_cases")} eventKey="active_cases">Active Cases</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("daily_active")} eventKey="daily_active">Daily Active Cases</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("total_recoveries")} eventKey="total_recoveries">Total Recoveries</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("daily_recoveries")} eventKey="daily_recoveries">Daily Recoveries</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("total_hospitalized")} eventKey="total_hospitalized">Total Hospitalized</ListGroup.Item>
+                <ListGroup.Item action onClick={() => this.menuClick("daily_hospitalized")} eventKey="daily_hospitalized">Daily Hospitalized</ListGroup.Item>
               </ListGroup>
               </Tab.Container>
             </Col>
