1e77d55a76b524d2fbcaa5672d399fc67ca99698
Adrian Selva <adrianjselva@gmail.com>
2020-07-07 13:10:58 -0700

    Daily update: 07/07/2020

--- pages/tn-covid.js
+++ pages/tn-covid.js
@@ -377,7 +377,7 @@ class TNCovid extends React.Component {
                 <span> Source: <a href="https://www.tn.gov/health/cedep/ncov.html"> Tennessee Department of Health</a> </span>
               </Row>
               <Row className="justify-content-center">
-                {"Last updated: July 6, 2020"}
+                {"Last updated: July 7, 2020"}
               </Row>
             </Container>
           </div>
