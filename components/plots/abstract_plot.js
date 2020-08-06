09afbdc235bf7a1df22721f5c53424089b0761c0
Adrian Selva <adrianjselva@gmail.com>
2020-08-06 11:26:32 -0500

    Added reporting delay disclaimer to specimen collection date plots

--- components/plots/abstract_plot.js
+++ components/plots/abstract_plot.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import plotly from 'plotly.js/dist/plotly';
 import createPlotComponent from 'react-plotly.js/factory';
+import Row from 'react-bootstrap/Row'
 
 const Plot = createPlotComponent(plotly);
 
@@ -20,6 +21,146 @@ class AbstractPlot extends React.Component {
         return this.testPlot(this.props.pObj);
       case "select":
         return this.select();
+      case "specimen":
+        return this.dailySpecimenPlot(this.props.pObj);
+    }
+  }
+
+  dailySpecimenPlot(pObj) {
+    let fourteenDayBegin = pObj.xval[(pObj.xval.length - 10)];
+    let fourteenDayEnd = pObj.xval[(pObj.xval.length - 1)];
+    let recHeight = Math.max(...pObj.yval);
+
+    let data = [{
+      x: pObj.xval,
+      y: pObj.yval,
+      type: 'bar',
+      mode: 'lines+markers',
+      marker: {
+        color: pObj.barcolor
+      },
+      hoverinfo: 'x+y'
+    },
+    {
+      x: pObj.xval,
+      y: pObj.movingAverage,
+      type: 'scatter',
+      mode: 'lines',
+      fill: 'tozeroy',
+      fillcolor: pObj.fillcolor,
+      line: {
+        color: pObj.movingLineColor
+      },
+      hoverinfo: 'skip'
+    }
+  ];
+
+    let layout = {
+      xaxis: {
+        title: "Date",
+      },
+      yaxis: {
+        title: pObj.ytitle,
+      },
+      showlegend: false,
+      margin: {
+        l: 60,
+        r: 2,
+        t: 25,
+        pad: 2
+      },
+      autosize: true,
+      dragmode: 'pan',
+      shapes: [
+        {
+          type: 'rect',
+          xref: 'x',
+          yref: 'y',
+          x0: fourteenDayBegin,
+          y0: 0,
+          x1: fourteenDayEnd,
+          y1: recHeight,
+          line: {
+            color: 'rgba(0, 0, 0, 0)'
+          },
+          fillcolor: 'rgba(156, 156, 156, 0.3)'
+        }
+      ]
+    };
+
+    return {
+      data: data,
+      layout: layout,
+      config: {
+        displaylogo: false,
+        responsive: false,
+        toImageButtonOptions: {
+          format: 'png',
+          filename: 'plot',
+          height: 900,
+          width: 1500,
+          scale: 2
+        },
+        modeBarButtonsToRemove: [
+          'hoverClosestCartesian',
+          'hoverCompareCartesian',
+          'select2d',
+          'lasso2d',
+          'autoScale2d'
+        ]
+      }
+    }
+  }
+
+  totalPlot(pObj) {
+    let data = [{
+      x: pObj.xval,
+      y: pObj.yval,
+      type: 'scatter',
+      mode: 'lines+markers',
+      line: {
+        color: pObj.linecolor
+      }
+    }];
+
+    let layout = {
+      xaxis: {
+        title: "Date",
+      },
+      yaxis: {
+        title: pObj.ytitle,
+      },
+      margin: {
+        r: 2,
+        l: 60,
+        t: 25,
+        pad: 2
+      },
+      autosize: true,
+      dragmode: 'pan'
+    };
+
+    return {
+      data: data,
+      layout: layout,
+      config: {
+        displaylogo: false,
+        responsive: false,
+        toImageButtonOptions: {
+          format: 'png',
+          filename: 'plot',
+          height: 900,
+          width: 1500,
+          scale: 2
+        },
+        modeBarButtonsToRemove: [
+          'hoverClosestCartesian',
+          'hoverCompareCartesian',
+          'select2d',
+          'lasso2d',
+          'autoScale2d'
+        ]
+      }
     }
   }
 
@@ -274,15 +415,23 @@ class AbstractPlot extends React.Component {
 
   render() {
     let pObj = this.plotObject(this.props.pObj);
+    let disclaimer = '';
+
+    if(this.props.pObj.type == "specimen") {
+      disclaimer = "* Recent dates are incomplete due to lags in reporting. The gray box corresponds to dates that are likely to not yet be reported completely.";
+    }
 
     return(
-      <Plot
-        data={pObj.data}
-        layout={pObj.layout}
-        config={pObj.config}
-        useResizeHandler={true}
-        style={{width: "100%", height: "100%"}}
-      />
+      <Row>
+        <Plot
+          data={pObj.data}
+          layout={pObj.layout}
+          config={pObj.config}
+          useResizeHandler={true}
+          style={{width: "100%", height: "100%"}}
+        />
+        <span>{disclaimer}</span>
+      </Row>
     );
   }
 }
